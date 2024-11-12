d2f6a870fc4a36b05a50905d043e0d5b0680f81b
A-Hoang1 <112354136+A-Hoang1@users.noreply.github.com>
2024-11-12 13:52:38 -0500

    Update rgb_to_hex.cc

--- programming files/rgb_to_hex.cc
+++ programming files/rgb_to_hex.cc
@@ -10,11 +10,11 @@ std::string rgb_to_hex(int r, int g, int b)
     g = std::max(0, std::min(255, g));
     b = std::max(0, std::min(255, b));
 //Using stringstream, ss is set equal to the hex value of the red, green, and blue values
-    std::stringstream ss;
+    std::stringstream sss;
     ss << std::uppercase << std::hex << std::setfill('0')
        << std::setw(2) << r << std::setw(2) << g << std::setw(2) << b;
 //C++ function setw is used to set the width
-    return ss.str();
+    return ss.str(r, g, b);
 }
 //Function returns the string.
 
