69be758b7837a586d770513b6f4561ace14638cc
lb26227 <112405474+lb26227@users.noreply.github.com>
2024-11-12 13:38:06 -0500

    Update sum_even_fibonacci.cc

--- programming files/sum_even_fibonacci.cc
+++ programming files/sum_even_fibonacci.cc
@@ -1,13 +1,15 @@
 #include <iostream>
-
+// program to add together every fibonacci number less than 400000
 int main()
 {
     int a = 0, b = 1;
     int sum_even = 0;
     while (b < 4000000) {
+        //if b is even add it to total sum 
         if (b % 2 == 0) {
             sum_even += b;
         }
+        //add the current integer to the previous integer
         int temp = b;
         b = a + b;
         a = temp;
