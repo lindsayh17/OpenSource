9bf2dd05a2a7778070c94a799d66e1613410c9e4
miacorcoran <148166140+miacorcoran@users.noreply.github.com>
2024-11-12 14:17:57 -0500

    Update datetime_convert.java Bug Fixed
    changed lines 6 and 11 '==' to '='

--- programming files/datetime_convert.java
+++ programming files/datetime_convert.java
@@ -3,12 +3,12 @@ import java.time.format.DateTimeFormatter;
 
 public class datetime_convert {
     public static void main(String[] args) {
-        string dateStr == "2022-03-17 10:45:30";
+        string dateStr = "2022-03-17 10:45:30";
         // format to year, month, day and time in hours, minutes, seconds
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         // local time format
         LocalDateTime dateObj = LocalDateTime.parse(dateStr, formatter);
-        String formattedDate == dateObj.format(DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss"));
+        String formattedDate = dateObj.format(DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss"));
         // print formatted date
         System.out.print(formattedDate);
     }
