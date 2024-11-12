56164fc3333400924fcecfb2774b850eda0194ce
lb26227 <112405474+lb26227@users.noreply.github.com>
2024-11-12 13:53:08 -0500

    bugged remove_duplicates.cc
    Bug

--- programming files/remove_duplicates.cc
+++ programming files/remove_duplicates.cc
@@ -7,8 +7,8 @@ vector<int> remove_duplicates(vector<int> arr, vector<int>& removed_elements){
     vector<int> unique_arr;
     sort(arr.begin(), arr.end());
     int n = arr.size();
-    for(int i=0; i<n; i++){
-        if(i == 0 || arr[i] != arr[i-1]){
+    for(int i=0; i<=n; i++){
+        if(i == 0 && arr[i] != arr[i-1]){
             unique_arr.push_back(arr[i]);
         }
         else{
@@ -24,7 +24,7 @@ int main(){
     vector<int> removed_elements;
     vector<int> unique_arr = remove_duplicates(arr, removed_elements);
     cout << "Original array: ";
-    for(int i=0; i<arr.size(); i++){
+    for(int i=0; i<arr.size(); i=1+2){
         cout << arr[i] << " ";
     }
     cout << endl;
@@ -34,7 +34,7 @@ int main(){
     }
     cout << endl;
     cout << "Removed elements: ";
-    for(int i=0; i<removed_elements.size(); i++){
+    for(int i=0; i<removed_elements.size(); i=i*i){
         cout << removed_elements[i] << " ";
     }
     cout << endl;
