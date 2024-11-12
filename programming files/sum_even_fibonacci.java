3e7d98969ff7bbaeb9edb0e5958b62a1e8ddba1f
Lindsay Hall <143295788+lindsayh17@users.noreply.github.com>
2024-11-12 13:52:34 -0500

    sum_even_fibonacci.java bug

--- programming files/sum_even_fibonacci.java
+++ programming files/sum_even_fibonacci.java
@@ -5,7 +5,7 @@ public class sum_even_fibonacci {
         //if b is even add it to total sum 
         while (b < 4000000) {
             if (b % 2 == 1) {
-                sumEven += b;
+                sumEven + b;
             }
             //add the current integer to the previous integer
             int temp = b;
