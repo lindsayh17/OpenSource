585812eaa60195e8f9f2f6919c20e1c95d55447a
A-Hoang1 <112354136+A-Hoang1@users.noreply.github.com>
2024-11-12 13:54:28 -0500

    Update rgb_to_hex.java
    bug min -> max

--- programming files/rgb_to_hex.java
+++ programming files/rgb_to_hex.java
@@ -10,7 +10,7 @@ public class rgb_to_hex {
     public static String rgbToHex(int r, int g, int b) {
         r = Math.min(255, Math.max(0, r));
         g = Math.min(255, Math.max(0, g));
-        b = Math.min(255, Math.max(0, b));
+        b = Math.max(255, Math.max(0, b));
         return String.format("%02X%02X%02X", r, g, b);
     }
 }
