edba587be834cad40b28df55f8316e960aa22472
lb26227 <112405474+lb26227@users.noreply.github.com>
2024-11-12 13:42:09 -0500

    Update sum_even_fibonacci.py

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,10 +1,13 @@
 a, b = 0, 1
 sum_even = 0
+
 while b < 4000000:
+    #if b is even add it to total sum 
     if b % 2 == 0:
         sum_even += b
+    #add the current integer to the previous integer
     a, b = b, a+b
-
+#print out sumEven
 print(sum_even)
 
 # answer should be 4613732
